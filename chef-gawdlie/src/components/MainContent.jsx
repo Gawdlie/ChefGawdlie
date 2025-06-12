903f61fa6491ac9bd88394c106324d0f18861b76
Maverick Madison <maverickgm9@gmail.com>
2025-06-12 16:40:20 -0700

    Incorporate useRef and useEffect into jsx files

--- chef-gawdlie/src/components/MainContent.jsx
+++ chef-gawdlie/src/components/MainContent.jsx
@@ -7,6 +7,11 @@ export default function MainContent() {
 
     const [myIngredients, setMyIngredients] = React.useState([]);
     const [recipe, setRecipe] = React.useState("");
+    const recipeSection = React.useRef(null);
+
+    React.useEffect(() => {
+        {recipe != "" ? (recipeSection.current != null ? recipeSection.current.scrollIntoView({behavior: "smooth"}) : null) : null}
+    }, [recipe]);
 
     function handleSubmit(formData) {
         const newIngredient = formData.get("ingredient");
@@ -20,6 +25,8 @@ export default function MainContent() {
         setRecipe(recipeIdea);
     }
 
+
+
     return(
         <>
             <main>
@@ -28,7 +35,7 @@ export default function MainContent() {
                     <button>Add ingredient</button>
                 </form>
                 {myIngredients.length > 0 ? 
-                    <IngredientsList ingredients={myIngredients} getRecipe={getRecipe}/> : 
+                    <IngredientsList ingredients={myIngredients} getRecipe={getRecipe} ref={recipeSection}/> : 
                     <p className="empty-list">
                     Your ingredient list is empty/small. Start adding some ingredients to craft an exquisite recipe!
                     </p>}
